595ce268d8afda4cc435ab94babb13e795927051
Martín Aberastegue <xyborg@gmail.com>
2025-02-16 14:59:19 +0100

    Added PWA functionality

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'safe-id-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/logo.png',
+  '/dni-before.png',
+  '/dni-after.png',
+  '/android-chrome-192x192.png',
+  '/android-chrome-512x512.png',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request)
+          .then(response => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              return response;
+            }
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(event.request, responseToCache);
+              });
+            return response;
+          });
+      })
+  );
+}); 
